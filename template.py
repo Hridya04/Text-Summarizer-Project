bcaa7b63388e08913aeb7712eb44cd10ce7b3acb
Hridya04 <hridyaramesh99@gmail.com>
2026-09-23 20:00:13 +0530

    data ingestion notebook added

--- template.py
+++ template.py
@@ -15,10 +15,10 @@ list_of_files = [
     f"src/{project_name}/utils/common.py",
     f"src/{project_name}/logging/__init__.py",
     f"src/{project_name}/config/__init__.py",
-    f"src/{project_name}/config/configuration/.py",
+    f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
-    f"src/{project_name}/constatnts/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "params.yaml",
     "app.py",
@@ -26,10 +26,10 @@ list_of_files = [
     "Dockerfile",
     "requirements.txt",
     "setup.py",
-    "research/trials.ipynb"
-
+    "research/trials.ipynb",
 
 ]
+
 
 for filepath in list_of_files:
     filepath = Path(filepath)
@@ -39,11 +39,13 @@ for filepath in list_of_files:
         os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creating directory:{filedir} for the file {filename}")
 
+    
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath,'w') as f:
             pass
             logging.info(f"Creating empty file: {filepath}")
 
 
-    else: 
-        logging.info(f"{filename} is already exists")            
+    
+    else:
+        logging.info(f"{filename} is already exists")
